a8f957c4242881e035c7c0daf0f8b760b25d0f94
2019success <kkt3759@naver.com>
2018-12-20 18:07:48 +0900

    SWEA 1946문제

---
+++ src/difficulty2/Solution1946.java
@@ -0,0 +1,31 @@
+package difficulty2;
+import java.io.*;
+
+public class Solution1946 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter((new OutputStreamWriter(System.out)));
+        int T = Integer.parseInt(br.readLine());
+        int i=1;
+        String result="";
+        while(i<=T){
+            int N = Integer.parseInt(br.readLine());
+            for(int j=0; j<N; j++) {
+                String input[] = br.readLine().split(" ");
+                for (int k = 0; k < Integer.parseInt(input[1]); k++) {
+                    result += input[0];
+                }
+            }
+            int count=0;
+            bw.write("#"+i+"\n");
+            for(int k=0; k<result.length()/10; k++){
+                bw.write(result.substring(k*10, (k+1)*10)+"\n");
+                count++;
+            }
+            bw.write(result.substring(count*10)+"\n");
+            result="";
+            i++;
+        }
+        bw.close();
+    }
+}

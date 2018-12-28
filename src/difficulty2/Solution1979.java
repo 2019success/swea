07bdc1b5dff799d31bb36598ae7d072631467395
2019success <kkt3759@naver.com>
2018-12-28 22:16:56 +0900

    SWEA 1979문제

--- src/difficulty2/Solution1979.java
+++ src/difficulty2/Solution1979.java
@@ -13,14 +13,38 @@ public class Solution1979 {
             String[] input = br.readLine().split(" ");
             int N = Integer.parseInt(input[0]);
             int K = Integer.parseInt(input[1]);
-            matrix = new int[N][N];
+            matrix = new int[N+1][N+1];
             for(int j=0; j<N; j++){
                 input = br.readLine().split(" ");
                 for(int k=0; k<N; k++){
                     matrix[j][k] = Integer.parseInt(input[k]);
                 }
             }
-
+            int rowSum, colSum;
+            int possible=0;
+            for(int j=0; j<N; j++){
+                rowSum=0;
+                colSum=0;
+                for(int k=0; k<N; k++){
+                    if(matrix[j][k]==0)
+                        rowSum=0;
+                    else
+                        rowSum++;
+                    if(matrix[k][j]==0)
+                        colSum=0;
+                    else
+                        colSum++;
+                    if(rowSum==K){
+                        if(matrix[j][k+1]==0)
+                            possible++;
+                    }
+                    if(colSum==K){
+                        if(matrix[k+1][j]==0)
+                            possible++;
+                    }
+                }
+            }
+            bw.write("#"+ i + " " + possible+ "\n");
             i++;
         }
         bw.close();

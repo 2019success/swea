ee87202af52c50d8a46ca620514ed2efbf1db6d1
2019success <kkt3759@naver.com>
2018-12-29 14:41:12 +0900

    SWEA 1928문제

---
+++ src/difficulty2/Solution1928.java
@@ -0,0 +1,21 @@
+package difficulty2;
+
+import java.io.*;
+import java.util.Base64;
+
+public class Solution1928 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        int i=1;
+        while(i<=T){
+            String strEncode = br.readLine();
+            String strDecode = new String(Base64.getDecoder().decode(strEncode));
+            bw.write("#" + i + " " + strDecode+"\n");
+            i++;
+        }
+        bw.close();
+    }
+}

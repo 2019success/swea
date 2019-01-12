0d894cb24c82dc27667e83a934c3d36c74d2c507
2019success <kkt3759@naver.com>
2019-01-12 22:28:21 +0900

    SWEA 4579문제

---
+++ src/difficulty3/Solution4579.java
@@ -0,0 +1,38 @@
+package difficulty3;
+
+import java.io.*;
+
+public class Solution4579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int i =1;
+        int T = Integer.parseInt(br.readLine());
+
+        while(i<=T){
+            String input = br.readLine();
+            String result = "Exist";
+            boolean flag=true;
+            int start=0;
+            int end=input.length()-1;
+            while(flag && start<input.length()/2){
+                if(input.charAt(start) == input.charAt(end)){
+                    start++;
+                    end--;
+                }
+                else if(input.charAt(start) =='*' || input.charAt(end) =='*'){
+                    break;
+                }
+                else{
+                    result = "Not exist";
+                    break;
+                }
+
+            }
+            bw.write("#"+ i + " " + result+"\n");
+            i++;
+        }
+        bw.close();
+    }
+}

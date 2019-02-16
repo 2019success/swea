982ee9f7415e31bb58092d6ba66470e17ddbed63
2019success <kkt3759@naver.com>
2019-02-16 16:13:52 +0900

    SWEA 1238문제

--- src/difficulty4/Solution1238.java
+++ src/difficulty4/Solution1238.java
@@ -13,7 +13,6 @@ public class Solution1238 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         int i =1;
         while(i<=10){
             matrix = new int[101][101];

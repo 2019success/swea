00b526335e5d51f297e7abe8fb8ce798d5df4e42
2019success <kkt3759@naver.com>
2019-03-08 14:45:30 +0900

    SWEA 7087문제

---
+++ src/difficulty3/Solution7087.java
@@ -0,0 +1,30 @@
+package difficulty3;
+
+import java.io.*;
+
+public class Solution7087 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int i =1;
+        int T = Integer.parseInt(br.readLine());
+        while(i<=T) {
+            int[] counts = new int[26];
+            int count=0;
+            int N = Integer.parseInt(br.readLine());
+            for(int j=0; j<N; j++){
+                String input = br.readLine();
+                char temp = input.charAt(0);
+                counts[temp-65]++;
+            }
+            for(int j=0; j<counts.length; j++){
+                if(counts[j]==0) break;
+                count++;
+            }
+            bw.write("#" + i + " " + count+"\n");
+            i++;
+        }
+        bw.close();
+    }
+}
